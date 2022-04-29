fc061c014e2672325412a02a80b7df8f7d867837
mishkamag <m93.maglaperidze@gmail.com>
2022-04-29 21:05:23 +0400

    new changes

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ var seats = document.querySelectorAll(".row .seat:not(.occupied)");
 var count = document.getElementById("count");
 var total = document.getElementById("total");
 var movieSelect = document.getElementById("movie");
+const checkout = document.querySelector(".checkout");
 
 var ticketPrise = +movieSelect.value;
 
 function updateSelectedCount() {
   var selectedSeats = document.querySelectorAll(".row .seat.selected");
-  /*var seatsIndex = [...selectedSeats].map(function (el) {
+  var seatsIndex = [...selectedSeats].map(function (el) {
     return [...seats].indexOf(el);
-  });ინდექსაციია ადგილების*/
+  });
+
+  checkout.innerHTML = ""; /*ყოველ რესტარტზე ასუფთავებს*/
+
+  seatsIndex.map((el) => {
+    const str = `Row ${Math.ceil(++el / 8)}.${el % 8}`;
+    const para = document.createElement("p");
+    para.innerText = str;
+    checkout.appendChild(para);
+  });
   var selectedSeatsCount = selectedSeats.length;
   count.textContent = selectedSeatsCount;
   total.innerText = "$" + ticketPrise * selectedSeatsCount;
